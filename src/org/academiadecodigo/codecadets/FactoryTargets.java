3aaaa0abc3a2e459ebc4ba506ff51090c8d60af8
DiogoVilaca <realplayerzero@gmail.com>
2018-10-05 19:26:39 +0000

    Changes to body of method createTarget and body of method createWeapon

--- src/org/academiadecodigo/codecadets/FactoryTargets.java
+++ src/org/academiadecodigo/codecadets/FactoryTargets.java
@@ -4,8 +4,15 @@ import org.academiadecodigo.codecadets.gameobjects.Target;
 
 public class FactoryTargets {
 
-    public static Target createTargets() {
-        Target target = new Target();
-        return target;
+    public static Enemy createEnemy() {
+        int random = (int) (Math.random() * EnemyType.values().length);
+        EnemyType enemyType = EnemyType.values()[random];
+        return enemy;
+    }
+
+    public static Props createProps(){
+        int random = (int) (Math.random() * PropType.values().lenght);
+        PropType propType = PropType.values()[random];
+        return prop;
     }
 }
