56b3b0d605c0e74d40fd9e1e4c34e70863f6c433
DiogoVilaca <realplayerzero@gmail.com>
2018-10-05 19:33:27 +0000

    Changes to createEnemy method

--- src/org/academiadecodigo/codecadets/FactoryTargets.java
+++ src/org/academiadecodigo/codecadets/FactoryTargets.java
@@ -7,8 +7,25 @@ public class FactoryTargets {
     public static Enemy createEnemy() {
         int random = (int) (Math.random() * EnemyType.values().length);
         EnemyType enemyType = EnemyType.values()[random];
+
+        Enemy enemy;
+
+        switch(enemyType){
+            case Enemy1:
+                enemy = new Enemy1();
+                break;
+
+            case Enemy2:
+                enemy = new Enemy2();
+                break;
+
+            default:
+                enemy = new Enemy1();
+        }
+
         return enemy;
     }
+
 
     public static Props createProps(){
         int random = (int) (Math.random() * PropType.values().lenght);
