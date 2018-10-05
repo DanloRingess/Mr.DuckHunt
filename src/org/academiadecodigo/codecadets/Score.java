173600fdfe37017103f124e074bc45f222908a1e
DiogoVilaca <realplayerzero@gmail.com>
2018-10-05 18:47:01 +0000

    Score Class properties and methods skeletons created

--- src/org/academiadecodigo/codecadets/Score.java
+++ src/org/academiadecodigo/codecadets/Score.java
@@ -2,5 +2,12 @@ package org.academiadecodigo.codecadets;
 
 public class Score {
 
+    private int score;
+
+    public int getScore(){
+        return 0;
+    }
+
+    public void changeScrore(int score){}
 
 }
