bdc141e849958431c96ea87fe11b2ec85eb9248e
César Silva <cesarsilva79@gmail.com>
2018-10-05 18:28:47 +0000

    class Position with getters and setters

--- src/org/academiadecodigo/codecadets/Position.java
+++ src/org/academiadecodigo/codecadets/Position.java
@@ -1,4 +1,23 @@
 package org.academiadecodigo.codecadets;
 
 public class Position {
+
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
