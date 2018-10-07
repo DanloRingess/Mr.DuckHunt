7dffc53f0c55d2eef056c92daa23ee8c796c5e32
JoaoSousa <joao.pd.sousa93@gmail.com>
2018-10-07 19:28:13 +0000

    Weapon/Enemy and Duck already done and ready to be used! - Shotgun class created

---
+++ src/org/academiadecodigo/codecadets/gameobjects/weapons/Shotgun.java
@@ -0,0 +1,14 @@
+package org.academiadecodigo.codecadets.gameobjects.weapons;
+
+import org.academiadecodigo.codecadets.enums.WeaponTypes;
+
+public class Shotgun extends Weapon{
+
+    public Shotgun(){
+        super(WeaponTypes.SHOTGUN);
+    }
+
+    public void move(int x, int y){
+        
+    }
+}
