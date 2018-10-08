5477fae8598818675e36a2fa8c5e8c6ed778438c
JoaoEnes <joaofenes@gmail.com>
2018-10-08 23:04:03 +0000

    Changed Terget.java to use the property speedY

--- src/org/academiadecodigo/codecadets/gameobjects/Target.java
+++ src/org/academiadecodigo/codecadets/gameobjects/Target.java
@@ -6,12 +6,13 @@ public abstract class Target extends GameObject {
 
     private boolean destroyed;
     private int speedX;
-    private int speedY = 0;
+    private int speedY;
 
     public Target() {
 
         destroyed = false;
         speedX = 2 + (int) Math.random()*4;
+        speedY = 0;
     }
 
     public abstract boolean hit(int damage);
@@ -26,9 +27,9 @@ public abstract class Target extends GameObject {
 
     public void move() {
 
-        Position newPos = new Position(this.getPosition().getX() + speedX, this.getPosition().getY());
+        Position newPos = new Position(this.getPosition().getX() + speedX, this.getPosition().getY() + speedY);
         this.setPosition(newPos);
 
-        this.getPicture().translate(speedX, 0);
+        this.getPicture().translate(speedX, speedY);
     }
 }
