f12bb184cf50231358227cce9101c61571d9d2a1
Roberto "ResaloliPT" Pires <roberto_pires97@sapo.pt>
2018-10-08 12:40:08 +0000

    Disabled sound, as MC_s will eventually teach us, added just to understand how it will work

--- src/org/academiadecodigo/codecadets/Main.java
+++ src/org/academiadecodigo/codecadets/Main.java
@@ -1,13 +1,13 @@
 package org.academiadecodigo.codecadets;
 
-import org.academiadecodigo.codecadets.sound.Sound;
-
 public class Main {
 
     public static void main(String[] args) {
         Game myGame = new Game();
-        Sound mySound = new Sound("resources/test.wav");
-        mySound.loop();
+
+        /*Sound mySound = new Sound("resources/test.wav");
+        mySound.loop();*/
+
         myGame.init("Hastronauts");
 
         myGame.gameStart();
--- src/org/academiadecodigo/codecadets/sound/Sound.java
+++ src/org/academiadecodigo/codecadets/sound/Sound.java
@@ -1,63 +1,49 @@
 package org.academiadecodigo.codecadets.sound;
 
+import javax.sound.sampled.*;
 import java.applet.AudioClip;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
+
+//Ignore this class It exist so we know how it may work, MC_s will *maybe* teach us to make it work
 public class Sound
         implements AudioClip
 {
     Clip clip;
 
-    public Sound(String paramString)
-    {
+    public Sound(String paramString) {
         URL localURL = Sound.class.getResource("/" + paramString);
-        try
-        {
-            if (localURL == null)
-            {
+        try {
+            if (localURL == null) {
                 File localFile = new File(paramString);
                 localURL = localFile.toURI().toURL();
             }
             AudioInputStream localAudioInputStream = AudioSystem.getAudioInputStream(localURL);
             this.clip = AudioSystem.getClip();
             this.clip.open(localAudioInputStream);
-        }
-        catch (UnsupportedAudioFileException localUnsupportedAudioFileException)
-        {
+        } catch (UnsupportedAudioFileException localUnsupportedAudioFileException) {
             localUnsupportedAudioFileException.printStackTrace();
-        }
-        catch (IOException localIOException)
-        {
+        } catch (IOException localIOException) {
             localIOException.printStackTrace();
-        }
-        catch (LineUnavailableException localLineUnavailableException)
-        {
+        } catch (LineUnavailableException localLineUnavailableException) {
             localLineUnavailableException.printStackTrace();
         }
     }
 
-    public void play()
-    {
+    public void play() {
         this.clip.stop();
         this.clip.flush();
         this.clip.setFramePosition(0);
         this.clip.start();
     }
 
-    public void loop()
-    {
+    public void loop() {
         this.clip.loop(-1);
     }
 
-    public void stop()
-    {
+    public void stop() {
         this.clip.stop();
     }
 }
