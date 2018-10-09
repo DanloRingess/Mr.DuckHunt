2c490b66f61178475366853835358fa7a2693a8f
JoaoEnes <joaofenes@gmail.com>
2018-10-09 00:36:24 +0000

    Checkout --

--- src/org/academiadecodigo/codecadets/gameobjects/enemies/Duck.java
+++ src/org/academiadecodigo/codecadets/gameobjects/enemies/Duck.java
@@ -13,7 +13,7 @@ public class Duck extends Enemy {
 
     public void init() {
 
-        setSpeedX((int) (5 + (Math.random() * 10)));
+        setSpeedX((int) (-10 + (Math.random() * 20)));
         setSpeedY(0);
 
         setPosition(new Position(0, 100 + (int) (Math.random() * 300)));
