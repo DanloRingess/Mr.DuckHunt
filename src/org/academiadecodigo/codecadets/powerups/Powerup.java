ecb19341cd5d8f6e7565492be4032b121f6ab946
Roberto "ResaloliPT" Pires <roberto_pires97@sapo.pt>
2018-10-11 20:10:39 +0000

    started props creation

---
+++ src/org/academiadecodigo/codecadets/powerups/Powerup.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.codecadets.powerups;
+
+import org.academiadecodigo.codecadets.enums.PowerUpTypes;
+
+public class Powerup {
+
+    PowerUpTypes powerUpType;
+
+    public Powerup(PowerUpTypes powerupType){
+        this.powerUpType = powerupType;
+    }
+
+}
